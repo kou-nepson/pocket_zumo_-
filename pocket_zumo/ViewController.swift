cb42fc08bd1befe791f7e3d45766233dbee4b870
杉山航 <S-jonnepuson@sugiyamawatarunomacbook-pro.local>
2018-01-31 14:26:08 +0900

    Initial Commit

---
+++ pocket_zumo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocket_zumo
+//
+//  Created by 杉山航 on 2018/01/31.
+//  Copyright © 2018年 杉山航. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
